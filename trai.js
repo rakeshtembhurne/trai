6e6eb08424bdde6751e9a3a0e9303faa271cc1ab
Andrew Parker <parker.andrew11@gmail.com>
2015-05-06 10:00:50 +0530

    Updated script to use line-by-line nodejs library

--- trai.js
+++ trai.js
@@ -1,10 +1,10 @@
+var LineByLineReader = require('line-by-line');    
+
 var cheerio = require("cheerio");
 var request = require("request");
 var mysql   = require("mysql");
-var async   = require("async");
 var colors  = require("colors");
-var fs      = require('fs');
-var http    = require('http');
+var async   = require("async");
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -36,8 +36,6 @@ var traiUrls = [
     'http://trai.gov.in/comments/24-April/24-April.html'
 ];
 
-var rowCount = 1000;
-
 var validName = function(name) {
     name = name.replace(/[^a-zA-Z0-9\s\d]/g, '');
     return name.trim();
@@ -48,75 +46,50 @@ var validEmail = function(email) {
     email = email.replace('(dot)', '.');
     return email;
 };
-var saveToDatabase = function(sql, rowCount, callback) {
+var saveToDatabase = function(sql, user, cb) {
     connection.query(sql, function(err, rows, fields) {
-        if (err) throw err;
-        console.log("Successfully added".green, rowCount, " rows".green);
-        if (callback) callback();
-    });
-};
-
-var download = function(url, dest, cb) {
-  var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);  // close() is async, call cb after close completes.
-    });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (cb) cb(err.message);
-  });  
-};
-
-var processFiles = function() {
-    async.eachSeries(traiUrls, function(url, callback) {
-        var rowCount = 1000;
-        console.log("==========", url, "==========");
-        var filename = url.substring(url.lastIndexOf('/')+1);
-        // var $ = cheerio.load(body);
-        var $ = cheerio.load(fs.readFileSync(filename));
-
-        var sqlCount = 0;
-        var sqlQuery = "INSERT INTO `users` (`name`, `email`) VALUES ";
-        $('tr').map(function(i, el) {
-            if (sqlCount >= 1000) {
-                sqlQuery = sqlQuery.replace(/,\s*$/, "");
-                saveToDatabase(sqlQuery, rowCount);
-                sqlQuery = "INSERT INTO `users` (`name`, `email`) VALUES ";
-                sqlCount = 0;
-                rowCount += 1000;
-            };
-            sqlCount++;
-            var nameEmail = $(this).children("td").first().next().text();
-            var match     = nameEmail.match(/(.*)<(.*\(at\).*(\(dot\))?.*)>?/);
-            if (match) {
-                sqlQuery += " ('"+validName(match[1])+"', '"+validEmail(match[2])+"'), ";
-            } else {
-                var match = nameEmail.match(/(.*\(at\).*(\(dot\))?.*)/);
-                if (match) {
-                    sqlQuery += " ('', '"+validEmail(match[1])+"'), ";
-                } else {
-                    console.log("Will not be saved: ".red, nameEmail);
-                }
-            } 
-        });
-
-        sqlQuery = sqlQuery.replace(/,\s*$/, "");
-        saveToDatabase(sqlQuery, rowCount, callback);        
-        
-    }, function(error) {
-        console.log("All urls are done, exiting now");
-        process.exit(0);
+        if (err) {
+            console.log(sql);
+            throw err;
+        }
+        console.log("Successfully added".green, user);   
+        cb();     
     });
 };
 
 async.eachSeries(traiUrls, function(url, callback) {
     var filename = url.substring(url.lastIndexOf('/')+1);
-    console.log(filename);
-    download(url, filename, callback);
-}, function(error) {
-    processFiles();    
-});
+    var lr       = new LineByLineReader(filename);
 
-//http://trai.gov.in/Comments/Comments-List003.pdf
+    lr.on('line', function (line) {
+        var $          = cheerio.load(line);
+        var nameEmail  = $("td").text().trim();
+        var didMatched = false;
+        var user = {name: '', email: ''};
+        var match = nameEmail.match(/(advqos)/);
+        if (!match) {
+            var match = nameEmail.match(/(.*)<(.*\(at\).*(\(dot\))?.*)>?/);
+            if (match) {
+                user.name  = validName(match[1]);
+                user.email = validEmail(match[2]);            
+                didMatched = true;
+            } else {
+                var match = nameEmail.match(/(.*\(at\).*(\(dot\))?.*)/);
+                if (match) user.email = validEmail(match[1]);
+            }
+        }
+
+        if (didMatched) {
+            var sqlQuery = "INSERT INTO `users` (`name`, `email`) VALUES ";
+            sqlQuery += " ('"+user.name+"', '"+user.email+"')";            
+            saveToDatabase(sqlQuery, user, callback);
+        }
+    });
+
+    lr.on('error', function (err) {
+        console.log(err);
+    });
+    lr.on('end', function () {
+        console.log("file has ended");
+    });
+});
